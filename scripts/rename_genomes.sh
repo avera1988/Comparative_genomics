eca8ff33e1bbb285a3b42fcef15a671c5b49e62f
avera1988 <36417540+avera1988@users.noreply.github.com>
2018-05-11 12:07:45 -0400

    Update rename_genomes.sh

--- scripts/rename_genomes.sh
+++ scripts/rename_genomes.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-#
+if [ $# -eq 0 ]; then
+        echo "usage: $0 list with genome_id in colum 0 and Organismis name in colum 1";
+        exit 1;
+        fi
 file=$1
 ls|grep 'GC' > genome_list.txt
 START=1;
@@ -7,7 +10,7 @@ END=$(cat lista_genomas.txt|wc -l);
 for ((c = $START ; c<= $END ; c++))
         do
         var=$(grep 'GC' $file|cut -f2|head -n ${c}|tail -n 1); dos=$(grep 'GC' $file|cut -f1|head -n ${c}|tail -n 1|tr '_' '\t'|cut
- -f 2|grep -f - lista_genomas.txt);
+ -f 2|grep -f - genome_list.txt);
 echo species ID:
 echo ${dos}
 echo Genome ID:
