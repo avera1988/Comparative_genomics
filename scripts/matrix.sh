67b31ae56f2ebde71e18fa61febb235ffd13b601
avera1988 <36417540+avera1988@users.noreply.github.com>
2018-05-22 17:13:41 -0400

    Update matrix.sh

--- scripts/matrix.sh
+++ scripts/matrix.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-        echo "usage: $0 list cat";
+        echo "usage: $0 list";
         exit 1;
         fi
+
 file=$1
-comon=$2
-dir=$(pwd); 
+#comon=$2
+dir=$(pwd);
+cat $file|sort > list.sort
 START=1;
-END=$(cat $file|wc -l);
+END=$(cat list.sort|wc -l);
 #echo $END
+echo "creating directories from list"
+while read -r line ;
+        do
+        name="$line" ;
+        mkdir $name.dir ;
+done < list.sort
+
 for ((c = $START ; c<= $END ; c++));
         do
-        a=$(head -n ${c} names|tail -n 1);
-        echo $a
-        mv $a*$comon*vs.*.out $a
+        a=$(head -n ${c} list.sort|tail -n 1);
+        echo "genomes used:" $a
+        mv $a*vs.*.out $a.dir
 done
 
 START=1;
-END=$(cat $file|wc -l);
-for ((c = $START ; c<= $END ; c++)); 
-	do 
-	a=$(head -n ${c} $file|tail -n 1); 
-	#echo $a
-	cd $a; 
-	matrix=$(for i in *.out ;do head $i|cut -f 1 ; done)
-	echo $matrix
-	cd $dir
-done
-
+END=$(cat list.sort|wc -l);
+for ((c = $START ; c<= $END ; c++));
+        do
+        a=$(head -n ${c} list.sort|tail -n 1);
+        #echo $a
+        cd $a.dir;
+        matrix=$(for i in *.out ;do head $i|cut -f 1 ; done)
+        echo $matrix
+        cd $dir
+done > matrix.tab
+echo "aai matrix is on:" matrix.tab
+list.sort |awk '{split($0,a,".cat"); print a[1]}' > matrix.names
