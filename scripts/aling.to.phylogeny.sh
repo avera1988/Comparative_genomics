118107f32626643c8f594639c7c792eba8ffa69f
avera1988 <36417540+avera1988@users.noreply.github.com>
2020-08-31 11:36:44 -0400

    Update aling.to.phylogeny.sh

--- scripts/aling.to.phylogeny.sh
+++ scripts/aling.to.phylogeny.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 cpu=$1
-scripts='/home/veraponcedeleon.1/bin/Comparative_genomics/scripts'
-multigenome='/home/veraponcedeleon.1/bin/multigenome2blocks/scripts'
+scripts='Comparative_genomics/scripts'
 
 for i in *.faa;
 	do
@@ -10,7 +9,7 @@ for i in *.faa;
 done
 bash $scripts/mafft.sh mod $cpu 
 cat *.alin > all.core.alin.fasta
-perl $multigenome/merge_seqs.pl all.core.alin.fasta > all.core.alin.fasta.merged
+perl merge_seqs.pl all.core.alin.fasta > all.core.alin.fasta.merged
 sed 's/*//g' all.core.alin.fasta.merged > all.core.alin.fasta.merged.sinstar
 mafft --thread $cpu all.core.alin.fasta.merged.sinstar > all.core.alin.fasta.merged.sinstar.alin.end
-$multigenome/Gblocks all.core.alin.fasta.merged.sinstar.alin.end g
+Gblocks all.core.alin.fasta.merged.sinstar.alin.end g
